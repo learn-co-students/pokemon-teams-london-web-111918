fb833a7a24046174d2602c5e7042ee1d21bd4aa1
Kevmsutton <kevmsutton@gmail.com>
2019-02-06 15:10:07 +0000

    completed

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -20,44 +20,60 @@ const state = {
 function renderTrainer(trainer){
   const divEl = document.createElement('div')
   ulEl = document.createElement('ul')
-  const tPokemons = trainer.pokemons.map(pokemon => `<li data-id=${pokemon.id}>${pokemon.nickname} (${pokemon.species}) <button data-id=${pokemon.id} class=release>release</button><li>`).join('')
+  const tPokemons = trainer.pokemons.map(pokemon => `<li data-id=${pokemon.id}>${pokemon.nickname} (${pokemon.species}) <button data-id=${pokemon.id} class="release">release</button><li>`).join('')
   divEl.className = 'card'
   divEl.dataset.id = trainer.id
   divEl.innerHTML = `
   <h1>${trainer.name}</h1>
-  <button data-trainer-id="${trainer.id}">Add Pokemon</button>`
+  <button data-id="${trainer.id}" class="delete">Add Pokemon</button>`
   ulEl.innerHTML = tPokemons
   mainEl.append(divEl)
   divEl.append(ulEl)
   ulEl.addEventListener('click', pokemonRelease)
+  divEl.addEventListener('click', addPokemon)
 }
 
 function renderTrainers(){
+  mainEl.innerHTML = ""
   state.trainers.forEach(trainer => renderTrainer(trainer))
 }
 
 
 const pokemonRelease = event => {
+  if (event.target.classList.contains("release")){
   const pokeId = event.target.dataset.id
   const flatPoke = state.trainers.map(trainers => trainers.pokemons).flat()
   const pokemon = flatPoke.find(pokemon => pokemon.id == pokeId)
   deletePokemonDB(pokemon.id)
+  }
+}
+
+const addPokemon = event => {
+  if (event.target.classList.contains("delete")){
+    id = event.target.dataset.id
+    trainer = state.trainers.find(trainer => trainer.id == id)
+    addAPokemon(trainer)
+    
+  }
 }
 
   function deletePokemonDB(id){
     return fetch(`http://localhost:3000/pokemons/${id}`, {
       method:'DELETE',
       headers: {'Content-Type': 'application/json'}
-    }).then(() => {
-      mainEl.innerHTML = ""
-      getTrainers()
-    })
+    }).then(() => getTrainers())
+  }
+
+  const addAPokemon = trainer => {
+    fetch(POKEMONS_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({trainer_id: `${trainer.id}`})
+    }).then(() => getTrainers())
   }
 
 
-
 getTrainers()
-// Fetch the trainers from the backend to populate state
-// Render all the trainers
-//    - Pass each trainer to a single trainer render function
-//    - Create the pokemon list items, and add event listener to the release button
+
